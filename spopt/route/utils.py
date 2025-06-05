a74a820748a67c06664d5cc6093b766400efc625
Dylan <dylan.skrah@gmail.com>
2025-06-05 16:54:04 -0700

    actually it was engine

--- spopt/route/utils.py
+++ spopt/route/utils.py
@@ -250,7 +250,7 @@ def routes_and_stops(
         route_obj = route_lut[name]
         group = group.sort_values("stop_number")
         coordinates = shapely.get_coordinates(group.geometry)
-        shape, durations = route.build_specific_route(
+        shape, durations = engine.build_specific_route(
             numpy.vstack(
                 (
                 depot_location,
