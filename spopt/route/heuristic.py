d399783e47af15cdad1a10c6cf1d6a3812d81820
Dylan <dylan.skrah@gmail.com>
2025-06-12 10:44:49 -0700

    working Guinness reproduction

--- spopt/route/heuristic.py
+++ spopt/route/heuristic.py
@@ -372,7 +372,7 @@ class LastMile:
         stops_for_map = self.stops_.copy()
         for col in stops_for_map.columns:
             if col != "geometry":
-                stops_for_map[col] = stops_for_map[col].astype(str)
+                stops_for_map[col] = self.stops_[col].astype(str)
         stops_for_map.explore(
             "route_name",
             m=m,
