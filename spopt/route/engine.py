a007dbf6fda72394588a27236f6ffc04b50bab85
Dylan <dylan.skrah@gmail.com>
2025-08-20 13:06:12 -0700

    spopt.route version 1.0

--- spopt/route/engine.py
+++ spopt/route/engine.py
@@ -14,23 +14,75 @@ from sklearn import metrics
 # TODO: needs to be configurable by site
 _OSRM_DATABASE_FILE = ""
 
-def build_route_table(demand_sites, candidate_depots, cost='distance', http=not has_bindings, database_path=_OSRM_DATABASE_FILE, port=5000):
-    """
-    Build a route table using OSRM, either over http or over py-osrm bindings
-    """
-    if isinstance(demand_sites, (geopandas.GeoSeries, geopandas.GeoDataFrame)):
-        demand_sites = demand_sites.geometry.get_coordinates().values
-    if isinstance(candidate_depots, (geopandas.GeoSeries, geopandas.GeoDataFrame)):
-        candidate_depots = candidate_depots.geometry.get_coordinates().values
-    if cost not in ("distance", "duration", "both"):
-        raise ValueError(f"cost option '{cost}' not one of the supported options, ('distance', 'duration', 'both')")
-    if http:
-        try: 
-            distances, durations = _build_route_table_http(demand_sites, candidate_depots, cost=cost, port=port)
-        except (requests.ConnectionError, requests.JSONDecodeError):
-            warnings.warn(
-                "Failed to connect to routing engine... using haversine distance"
-                " and (d/500)**.75 for durations"
+def build_specific_route(waypoints,
+                         return_durations=True,
+                         routing=None,
+                         **kwargs):
+    
+    '''
+    Parameters
+    ----------
+    
+    waypoints : list, required
+         A list of coordinate pairs between which to path a vehicular route.
+         The coordinates are also expected as a list.
+         e.g.: [[-6.2288162, 53.365756], [-6.2652379, 53.330686]].
+    
+    return_durations : boolean, required
+         Set to ``True`` to return durations for each leg.
+         Default is ``True``.
+
+    routing : dictionary, optional
+        Specifies which engine and associated parameters to utilize for the request.
+        Supported engines:
+            OSRM - Open Source Routing Machine
+
+    Returns
+    -------
+    
+    route_shape : geometry
+        A linestring reflecting the shortest path between the inputed waypoints.
+
+    leg_duration : numpy.array
+        An array of the durations on each leg of the route.
+    '''
+    engine = routing if routing is not None else kwargs.get("routing", None)
+    
+    if isinstance(engine, OSRM):
+        directions = engine.directions(
+            locations=waypoints,
+            geometries='geojson',
+            annotations=True
+        )
+        
+    elif isinstance(engine, Valhalla):
+        profile = kwargs.get("profile")
+        directions = engine.directions(
+            locations=waypoints,
+            geometries='geojson',
+            annotations=True,
+            profile=profile
+        )
+        
+    elif engine is None:
+        route_shape = shapely.LineString(waypoints)
+        prep_points = numpy.fliplr(numpy.deg2rad(numpy.array(waypoints, dtype=float)))
+        leg_durations = numpy.array([
+            (metrics.pairwise.haversine_distances([prep_points[i]], [prep_points[i + 1]]) * 637000 / 10)
+            for i in range(len(prep_points) - 1)
+        ])
+
+    else:
+        raise ValueError(f"Unsupported routing engine type: {type(engine)}")
+        
+    if isinstance (engine, (OSRM, Valhalla)):
+        route_coords = directions.geometry  # List of (lon, lat)
+        route_shape = shapely.LineString(route_coords)
+        legs = directions.raw['routes'][0]['legs']
+        leg_durations = numpy.array([leg['duration'] for leg in legs])
+        numpy.testing.assert_array_equal(
+            len(legs),
+            len(waypoints) - 1
             )
             distances = metrics.pairwise_distances(
                     numpy.fliplr(numpy.deg2rad(demand_sites)), 
@@ -140,51 +192,64 @@ def _build_specific_route_http(waypoints, return_durations=True, port=5000):
 def _build_specific_route_pyosrm(waypoints, database_path=_OSRM_DATABASE_FILE, return_durations=False):
     raise NotImplementedError()
 
-def _build_route_table_http(demand_sites, candidate_depots, cost='distance', port=5000):
-    """
-    Build a route table using the http interface to the OSRM engine
-    """
-    request_url = _create_route_request(demand_sites, candidate_depots, cost=cost, port=port)
-    request = requests.get(request_url)
-    content = request.json()
-    if cost == 'distance':
-        D = numpy.asarray(content["distances"]).astype(float)
-        output = (D,None)
-    elif cost == 'duration':
-        D = numpy.asarray(content["durations"]).astype(float)
-        output = (None,D)
-    elif cost == 'both':
-        distances = numpy.asarray(content["distances"]).astype(float)
-        durations = numpy.asarray(content["durations"]).astype(float)
-        output = (distances, durations)
+def build_route_table(demand_sites,
+                      candidate_depots,
+                      cost='distance',
+                      **kwargs):
+    
+    '''
+    parameters:
+    demand_sites = a list of coordinates pairs for clients. The coordinates are also expected 
+    as a list, e.g.: [[-6.2288162, 53.365756], [-6.2652379, 53.330686]]
+    
+    candidate_depots = a list of coordinate pairs for depot(s).The coordinates are also expected 
+    as a list, e.g.: [[-6.2288162, 53.365756]
+    
+    returns tuple (distance_matrix, duration_matrix)
+
+    
+    '''
+    engine = kwargs.get("routing", None)
+    routing_kws = kwargs.get("routing_kws", {})
+                       
+    candidate_series = pandas.Series([tuple(coord) for coord in candidate_depots])
+    demand_series = pandas.Series([tuple(coord) for coord in demand_sites])
+    all_points = pandas.concat((candidate_series, demand_series)).reset_index(drop=True)
+    
+    if engine is None:
+    # no engine is provided
+    
+        warnings.warn(
+                "Failed to connect to routing engine... using haversine distance"
+                "durations = 0"
+                    )
+        
+        coords_array = numpy.array(all_points.to_list(), dtype=float) # needed for deg2rad
+        distances = metrics.pairwise_distances(
+                    numpy.fliplr(numpy.deg2rad(coords_array)), 
+                    numpy.fliplr(numpy.deg2rad(coords_array)),
+                    metric="haversine"
+                ) * 6371000
+        durations = numpy.zeros_like(distances) # set all durations to match distances (0)
+
     else:
         raise ValueError(f"cost option '{cost}' not one of the supported options, ('distance', 'duration', 'both')")
     return output
 
-
-def _create_route_request(demand_sites, candidate_depots, cost='distance', port=5000):
-    point_string = ";".join(
-        map(
-            lambda x: "{},{}".format(*x),
-            numpy.row_stack((candidate_depots, demand_sites)),
-        )
-    )
-    n_demands = len(demand_sites)
-    n_supplys = len(candidate_depots)
-    source_string = "sources=" + ";".join(numpy.arange(n_supplys).astype(str))
-    destination_string = "destinations=" + ";".join(
-        numpy.arange(n_supplys, n_demands + n_supplys).astype(str)
-    )
-    # TODO: needs to be configurable by site
-    baseurl = f"http://127.0.0.1:{int(port)}/table/v1/driving/"
-    if cost=='distance':
-        annotation = "&annotations=distance"
-    elif cost=='duration':
-        annotation = "&annotations=duration"
-    elif cost=='both':
-        annotation = "&annotations=duration,distance"
-    else:
-        annotation = ""
+        if isinstance(engine, OSRM):
+            result = engine.matrix(
+                locations=all_points,
+                annotations=annotations,
+            )
+            
+        elif isinstance(engine, Valhalla):
+            profile = routing_kws.get("profile")
+            print(f'routing profile: {profile}')
+            result = engine.matrix(
+                locations=all_points,
+                annotations=annotations,
+                profile=profile
+            )
 
     request_url = (
         baseurl
--- spopt/route/utils.py
+++ spopt/route/utils.py
@@ -250,7 +250,8 @@ def routes_and_stops(
         route_obj = route_lut[name]
         group = group.sort_values("stop_number")
         coordinates = shapely.get_coordinates(group.geometry)
-        shape, durations = routing.build_specific_route(
+        print("[routes_and_stops] routing passed type:", type(kwargs.get("routing", None)))
+        shape, durations = engine.build_specific_route(
             numpy.vstack(
                 (
                 depot_location,
